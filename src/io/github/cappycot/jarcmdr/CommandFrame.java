f89ef4434e065ed4fad4930d351b05127001d82f
Chris Wang <chriswang444@gmail.com>
2015-09-14 21:17:14 -0500

    Creating sh*t...

--- src/io/github/cappycot/jarcmdr/CommandFrame.java
+++ src/io/github/cappycot/jarcmdr/CommandFrame.java
@@ -1,35 +1,55 @@
 package io.github.cappycot.jarcmdr;
 
-import java.awt.EventQueue;
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class CommandFrame extends JFrame {
 	// Default Settings //
 	private static final long serialVersionUID = -5000732405037534401L;
+	private JTextField inputField;
+	private JTextField statusField;
+	private JScrollPane scrollPane;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CommandFrame frame = new CommandFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		CommandFrame frame = new CommandFrame();
+		frame.setVisible(true);
 	}
 
 	/**
 	 * Create the frame.
 	 */
 	public CommandFrame() {
+		setIconImage(Toolkit
+				.getDefaultToolkit()
+				.getImage(
+						CommandFrame.class
+								.getResource("/com/sun/java/swing/plaf/windows/icons/Computer.gif")));
+		setTitle("Jar Commander V0.0.1");
 		setBounds(100, 100, 400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		inputField = new JTextField();
+		inputField.setToolTipText("System.in");
+		getContentPane().add(inputField, BorderLayout.SOUTH);
+		inputField.setColumns(10);
+
+		scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+		statusField = new JTextField();
+		statusField.setHorizontalAlignment(SwingConstants.CENTER);
+		statusField.setText("Status Bar");
+		statusField.setEditable(false);
+		getContentPane().add(statusField, BorderLayout.NORTH);
+		statusField.setColumns(10);
 	}
 
 }
